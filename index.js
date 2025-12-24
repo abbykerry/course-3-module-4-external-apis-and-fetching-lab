bb764f7f513a068de104e3af98857795070281ff
Abigael Ongwae <abbykerry17@gmail.com>
2025-12-24 22:34:35 +0300

    added changes to index.js

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 // index.js
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('state-input')
+    const button = document.getElementById('fetch-alerts')
+    const alertsDisplay = document.getElementById('alerts-display')
+    const errorDiv = document.getElementById('error-message')
+
+    button.addEventListener('click', () => {
+        const state = input.value
+        
+        fetch(weatherApi + state)
+        .then(response => response.json())
+        .then(data => {
+            errorDiv.textContent = ''
+            errorDiv.classList.add('hidden')
+
+            alertsDisplay.textContent = ''
+            const title = data.title
+            const count = data.features.length
+            const summary = document.createElement('h2')
+            summary.textContent = `${title}: ${count}`
+            alertsDisplay.appendChild(summary)
+            
+            data.features.forEach(alert => {
+                const p = document.createElement('p')
+                p.textContent = alert.properties.headline
+                alertsDisplay.appendChild(p)
+})
+})
+        .catch(error => {
+            errorDiv.textContent = error.message
+            errorDiv.classList.remove('hidden')
+        })
+
+  input.value = ''
+
+})
+
+
+})
+
+
 // Your code here!
